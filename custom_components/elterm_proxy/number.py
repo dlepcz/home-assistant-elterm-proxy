c8526997b34fb767f24e37004ca99409144d1217
domin <dominik.lepczynski@gmail.com>
2025-04-08 13:03:57 +0200

    Bug

--- custom_components/elterm_proxy/number.py
+++ custom_components/elterm_proxy/number.py
@@ -33,14 +33,9 @@ class EltermBoilerNumber(EltermEntity, NumberEntity):
         self.entity_description = description
         self._attr_has_entity_name = True
         self._attr_unique_id = f"{self.proxy.name}_{description.key}"
-        self._register = description.register
         self._attr_native_min_value = description.attrs["min"]
         self._attr_native_max_value = description.attrs["max"]
         self._attr_native_value = description.attrs["default"]
-
-    @property
-    def current_option(self) -> str:
-        return int(self.proxy.boiler_temp) / 100
     
     async def async_set_native_value(self, value: float) -> None:
         self.proxy.boiler_temp = str(value * 100)
