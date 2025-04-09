ec1ac11b1e692b8b9f84304033b9f7f631bf9fae
domin <dominik.lepczynski@gmail.com>
2025-04-09 19:56:37 +0200

    sensor data not updated

--- custom_components/elterm_proxy/sensor.py
+++ custom_components/elterm_proxy/sensor.py
@@ -7,10 +7,11 @@ from homeassistant.components.sensor import (
     SensorEntity,
     SensorEntityDescription,
 )
+from homeassistant.core import HomeAssistant, callback
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_entities):
     proxy = hass.data[DOMAIN][config_entry.entry_id]
 
     entities = []
@@ -29,3 +30,11 @@ class EltermProxySensor(EltermEntity, SensorEntity):
         self.entity_description = description
         self._attr_has_entity_name = True
         self._attr_unique_id = f"{self.proxy.name}_{description.key}"
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        new_value = self.proxy.elterm_data.get(self.entity_description.key)
+        _LOGGER.debug("Update sensor %s to %s", self.entity_description.key, new_value)
+        self._attr_native_value = new_value
+
+        super()._handle_coordinator_update()
